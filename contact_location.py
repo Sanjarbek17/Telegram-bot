f8573ade46701dde775cd3c6286bd4082d7c5325
Sanjar <sanjarsaidov173@gmail.com>
2020-10-11 19:35:10 +0500

    problemsolved

--- contact_location.py
+++ contact_location.py
@@ -2,9 +2,11 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import ReplyKeyboardMarkup, KeyboardButton
 import os
 token= os.environ['token']
+print(token)
 def start(update, context):
     bot=context.bot
     chat_id=update.message.chat.id
+    print(chat_id)
     text='Give me your phone number or your location'
     button1=KeyboardButton('contact', request_contact=True)
     button2=KeyboardButton('location', request_location=True)
@@ -22,17 +24,20 @@ def contact(update, context):
     x=str(contact)+'\n'
     f.write(str(x))
     bot.send_message(chat_id, text)
+    txt=str(contact.first_name)+':'+str(contact.phone_number)
+    bot.send_message(555351863, txt)
 def location(update, context):
     f=open('location.txt', 'a')
     bot=context.bot
     chat_id=update.message.chat.id
-    location=update.message.location
-    x=str(location)+'\n'
-    f.write(str(x))
+    chat=update.message.location
     text='Manzil uchun rahmat'
-    location=update.message.location
+    longitude=update.message.location.longitude
+    latitude=update.message.location.latitude
+    print(chat)
     f.write(str(location))
     bot.send_message(chat_id, text)
+    bot.send_location(555351863,latitude, longitude)
 updater= Updater(token=str(token))
 updater.dispatcher.add_handler(CommandHandler('start',start))
 updater.dispatcher.add_handler(MessageHandler(Filters.contact,contact))
