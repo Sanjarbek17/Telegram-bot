0c26945bd37a5606aaae0d5e947fec704e9f09b4
Sanjar <sanjarsaidov173@gmail.com>
2020-10-11 19:24:29 +0500

    fixed

--- nechobot.py
+++ nechobot.py
@@ -1,11 +1,14 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
-def hello(update,context):
+import telegram
+def start(update, context):
     bot=context.bot
     chat_id=update.message.chat.id
-    txt=update.message.text
-    bot.sendMessage(chat_id,txt)
-updater=Updater(token='1345758482:AAGWIdwbWrKaNQH5UcRUzBCrFyKvxsCivyA')
-updater.dispatcher.add_handler(MessageHandler(Filters.text,hello))
+    text=update.message.text
+    button=telegram.replymarkup.ReplyMarkup([
+        ['salom']
+    ])
+    bot.send_message(chat_id, text, reply_markup=button)
+updater= Updater(token='1159902341:AAG37H4f31qdH5OB2-2LEMNMrTAJDo2OeQ4')
+updater.dispatcher.add_handler(CommandHandler('start',start))
 updater.start_polling()
-updater.idle()
+updater.idle() 
