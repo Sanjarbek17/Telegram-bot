467e8c6c5b2c5df577db94c30a3ea7f09dcc185e
Sanjar <sanjarsaidov173@gmail.com>
2020-10-11 18:41:30 +0500

    contact_Location

---
+++ contact_location.py
@@ -0,0 +1,38 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ReplyKeyboardMarkup, KeyboardButton
+import os
+token= os.environ['token']
+print(token)
+def start(update, context):
+    bot=context.bot
+    chat_id=update.message.chat.id
+    text='Give me your phone number or your location'
+    button1=KeyboardButton('contact', request_contact=True)
+    button2=KeyboardButton('location', request_location=True)
+    button=ReplyKeyboardMarkup([
+        [button1],
+        [button2]
+    ], resize_keyboard=True)
+    bot.send_message(chat_id, text, reply_markup=button)
+def contact(update, context):
+    f=open('contact.txt', 'a')
+    bot=context.bot
+    chat_id=update.message.chat.id
+    text='Telefon raqam uchun rahmat'
+    contact=update.message.contact
+    f.write(str(contact))
+    bot.send_message(chat_id, text)
+def location(update, context):
+    f=open('location.txt', 'a')
+    bot=context.bot
+    chat_id=update.message.chat.id
+    text='Manzil uchun rahmat'
+    location=update.message.location
+    f.write(str(location))
+    bot.send_message(chat_id, text)
+updater= Updater(token=str(token))
+updater.dispatcher.add_handler(CommandHandler('start',start))
+updater.dispatcher.add_handler(MessageHandler(Filters.contact,contact))
+updater.dispatcher.add_handler(MessageHandler(Filters.location,location))
+updater.start_polling()
+updater.idle() 
