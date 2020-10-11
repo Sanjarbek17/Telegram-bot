fc6a3f1d81d90dbafdfae9a29b8454e4549e8486
Sanjar <sanjarsaidov173@gmail.com>
2020-10-11 19:23:54 +0500

    fixed

--- echo_bot.py
+++ echo_bot.py
@@ -1,8 +1,13 @@
 import telegram
 from pprint import pprint
-bot = telegram.Bot(token='1345758482:AAGWIdwbWrKaNQH5UcRUzBCrFyKvxsCivyA')
+bot = telegram.Bot(token='1175464841:AAETPy_hg3fOfvTuLgrpvJd3Fzkde5r9PZQ')
+lastid=-1
 while True:
     update=bot.getUpdates()[-1]
     chat_id=update.message.chat.id
     text=update.message.text
-    bot.send_message(chat_id,text)
+    bid=update.update_id
+    if lastid!=bid:
+        print(text)
+        bot.send_message(chat_id,text)
+        lastid=bid
